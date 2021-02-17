d944aea444e27690811babebef29ff5cf44ad226
Smith Thay <contact.smith.thay@gmail.com>
2021-02-17 13:23:26 -0500

    functional code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+setInterval(() =>{
 
 const time = document.getElementById("time");
 
@@ -7,7 +7,22 @@ let date = new Date();
 let hrs = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-let half = "PM"
+let half = "AM"
 
+if(hrs > 12){
+    hrs = hrs - 12;
+    half = "PM"
+}
+if(hrs < 10){
+    hrs = '0' + hrs 
+}
+if(min < 10){
+    min = '0' + min;
+}
+if(sec < 10){
+    sec = '0' + sec;
+}
 
-time.textContent = hrs + ':' + min + ':' + sec + ':' + half
+time.textContent = hrs + ':' + min + ':' + sec + ' ' + half;
+
+}, 1000);
